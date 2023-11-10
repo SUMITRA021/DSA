1b0a2e11b4a83145addb54c4dce9d4008df978cf
SUMITRA021 <dawnsumitra021@gmail.com>
2023-11-11 00:31:05 +0530

    program1

---
+++ demo.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Sumi"; cout << "sumi";
+    return 0;
+}
---
+++ p1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 50;
+    char ch = 'k';
+    cout << num<<endl;
+    cout << sizeof(num) << endl;
+
+    cout << ch<<endl;
+    cout << sizeof(ch) << endl;
+
+    return 0;
+}
