ef6ac6dc2ebbe61f4af98353ea749d10f73d7815
SUMITRA021 <dawnsumitra021@gmail.com>
2023-11-14 12:30:22 +0530

    operator

---
+++ operator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int op1 = 5;
+    int op2 = 10;
+    cout << op1 + op2 << endl;
+    cout << op1 - op2 << endl;
+    cout << op1 * op2 << endl;
+    cout << op2 / op1 << endl;
+}
