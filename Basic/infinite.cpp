84bcd9197a63bb208815f113e8cc9c2e388e17bf
SUMITRA021 <dawnsumitra021@gmail.com>
2023-11-15 19:04:25 +0530

    Pattern

---
+++ Basic/infinite.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    for (;;)
+    {
+        if(i<5)
+        {
+            cout << "Sumitra" << endl;
+            i = i + 1;
+        }
+    }
+    cout << "end of loop";
+    return 0;
+}
