e7d7118f4f71e3b6178bc02660412c48f84c5fd0
SUMITRA021 <dawnsumitra021@gmail.com>
2023-11-15 21:58:23 +0530

    shiftoperator

--- Bitwise/bitwiseOp.cpp
+++ Bitwise/bitwiseOp.cpp
@@ -14,5 +14,28 @@ int main()
     cout << ~(b) << endl;
     cout << (~n) << endl;
     cout << (a ^ b) << endl;
+
+
+//left and right shift
+
+    int a1 = 7;
+    cout << (a1 << 1) << endl;
+    cout << (a1 << 2) << endl;
+    cout << (a1 << 3) << endl;
+
+    int a2 = 50;
+    cout << (a2 >> 1) << endl;
+    cout << (a2 >> 2) << endl;
+    cout << (a2 >> 3) << endl;
+
+    int n1 = -100;
+    cout << (n1 >> 1) << endl;
+
+    unsigned int n2 = -100;
+    cout << (n2 >> 1) << endl;
+
+    int n3 = 10;
+    cout << (n3 << -1)<<endl;
+
     return 0;
 }
