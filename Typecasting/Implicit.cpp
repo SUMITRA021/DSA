5e48c771732c4ea14db5207be3fa31b7ac4ba169
SUMITRA021 <dawnsumitra021@gmail.com>
2023-11-16 12:58:11 +0530

    implicit

--- Typecasting/Implicit.cpp
+++ Typecasting/Implicit.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 int main()
 {
-    cout << "hello";
+
+    //int to float
+    int num1 = 10;
+    float num2 = 5.5;
+    float result = num1 + num2;
+    cout << result << endl;
+
+    //char to int
+    char ch = 'A';
+    int a = ch + 1;
+    cout << a << endl;
+
+    //char to int
+    char chH = 'A';
+    char b = ch + 1;
+    cout << b << endl;
+
+
+    //int to char
+    int d = 97;
+    char chhh = d;
+    cout << chhh << endl;
+
     return 0;
 }
