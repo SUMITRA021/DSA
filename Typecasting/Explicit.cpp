44b5c4b43a173c182e553b7788e35282e5400653
SUMITRA021 <dawnsumitra021@gmail.com>
2023-11-16 13:16:38 +0530

    explicit

--- Typecasting/Explicit.cpp
+++ Typecasting/Explicit.cpp
@@ -1,8 +1,21 @@
+//coder do the conversion manually
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    //float to int
+    int num1 = 10;
+    float num2 = 5.5;
+    float result = num1 + (int)num2;  // ()-> casting operator
+    cout << result << endl;
+
+
+    //float to char
+    float f1 = 65.35;
+    char ch = char(f1);
+    cout << ch << endl;
 
     return 0;
 }
