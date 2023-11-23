1451978b138e9fcc3a6a253e09680b32d861ba2e
SUMITRA021 <dawnsumitra021@gmail.com>
2023-11-24 00:28:38 +0530

    triplet

--- Arrays/Triplets.cpp
+++ Arrays/Triplets.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 using namespace std;
 
-void PrintPair(int arr[],int size)
+void PrintTriplet(int arr[],int size)
 {
     for (int i = 0; i < size;i++)
     {
         for (int j = 0; j < size;j++)
         {
-            cout << arr[i] <<", "<< arr[j] <<endl;
+            for (int k = 0; k < size;k++)
+            {
+                cout << arr[i] <<", "<< arr[j] <<", "<<arr[k]<<endl;
+            }
         }
     }
 
@@ -25,8 +28,8 @@ void PrintPair(int arr[],int size)
 
 int main()
 {
-    int arr[] = {10, 20, 30};
-    int size = 3;
-    PrintPair(arr, size);
+    int arr[] = {1,2,3,4};
+    int size = 4;
+    PrintTriplet(arr, size);
     return 0;
 }
