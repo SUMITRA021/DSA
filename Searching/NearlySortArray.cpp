b4c1e6f67c144c4c081cb8ed553402f57eb4124e
SUMITRA021 <dawnsumitra021@gmail.com>
2023-12-06 19:29:16 +0530

    NearlySortedArray

---
+++ Searching/NearlySortArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int nearlySort(int arr[],int target,int n)
+{
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    while(s<=e)
+    {
+        if(mid-1>=0 && arr[mid-1]==target){
+            return (mid - 1);}
+        else if(arr[mid]==target){
+            return mid;}
+        else if(mid+1<n && arr[mid+1]==target){
+            return (mid + 1);}
+        else if(arr[mid]<target){
+            s = mid + 2;}
+        else{
+            e = mid - 2;}
+        mid = s + (e - s) / 2;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {20, 10, 30, 50, 40, 70, 60};
+    int n = 7;
+    int target = 20;
+    int res = nearlySort(arr,target,n);
+    cout << "Index of target:" << res << endl;
+    return 0;
+}
