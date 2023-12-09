12ef0340a6f1761f2170e923586ae7f02ec91abc
SUMITRA021 <dawnsumitra021@gmail.com>
2023-12-09 13:22:13 +0530

    insertion

--- Sorting/Insertion.cpp
+++ Sorting/Insertion.cpp
@@ -4,22 +4,37 @@
 using namespace std;
 
 void print(vector<int> &v) {
-  for (int i = 0; i < v.size(); ++i) {
-    cout << v[i] << " ";
-  }
+//   for (int i = 0; i < v.size(); ++i) {
+//     cout << v[i] << " ";
+//   }
+    for(auto it:v)
+    {
+        cout << it <<" ";
+    }
   cout << endl;
 }
 
 void insertionSort(vector<int>&v)
 {
-    
+    int n = v.size();
+    for (int i = 1; i < n ;i++) 
+    {
+        int key = v[i];
+        int j = i - 1;
+        while(j>=0 && v[j]>key)
+        {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = key;
+    }
 }
 
 int main() {
   vector<int> v = {44,33,55,22,11};
   // bubbleSort(v);
   // selectionSort(v);
-  //insertionSort(v);
+  insertionSort(v);
   print(v);
   return 0;
 }
