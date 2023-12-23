02d121219ff545fcc5005a2c9c30e0d8c839a7a6
Sumitra Dawn <dawnsumitra021@gmail.com>
2023-12-23 21:22:41 +0530

    replacestring

---
+++ Arrays/char array/ReplaceWithChar.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+void ReplaceString(char ch[],int size)
+{
+    int index = 0;
+    while(ch[index]!='\0')
+    {
+        if(ch[index] == '@')
+            ch[index] = ' ';
+        index++;
+    }
+}
+
+
+int main()
+{
+    char ch[100];
+    cin.getline(ch,100);
+    cout << "Before:" << ch <<endl;
+    //int len = strlen(ch);
+    ReplaceString(ch, 100);
+    cout << "After:" << ch<<endl;
+
+        return 0;
+}
